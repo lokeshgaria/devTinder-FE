1e22f4c5948c86ce8151439ea8e553a882d540f4
lokeshgaria <lokeshgaria8811@gmail.com>
2026-01-03 23:50:26 +0530

    added changes

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { showError } from "./notifications";
 
 const AXIOS_API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "/",
   withCredentials: true,
   timeout: 15000,
   headers: {
